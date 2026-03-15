c54cd20845df56faf86510b2eaea4e55ceb1e7c8
Kevin <kevinflotow@arcor.de>
2026-03-15 17:08:19 +0100

    functional upgrade for criticalThinking component

--- frontend/src/components/pillar-components/CriticakThinking.jsx
+++ frontend/src/components/pillar-components/CriticakThinking.jsx
@@ -9,10 +9,14 @@ import DataQuality from '../../assets/pillarPages/critical-thinking/data-quality
 import Accountability from '../../assets/pillarPages/critical-thinking/accountability.png';
 
 
-
 function CriticalThinking() {
 
     const [activeInfo , setActiveInfo] = useState('');
+    const [visible , setVisibility] = useState(false);
+
+    const showInfo = () => {
+        setVisibility(true);
+    }
 
     return(
 
@@ -32,19 +36,38 @@ function CriticalThinking() {
                 <div className="critical-fields">
 
                     <img src={Accuracy} alt="accuracy-img" className="critical-img"
-                        onClick={() => setActiveInfo('Accuracy - Verify whether the AI’s information is correct and supported by reliable sources.')}/>
+                        onClick={() => {
+                            setActiveInfo('Accuracy - Verify whether the AI’s information is correct and supported by reliable sources.');
+                            showInfo();
+                        }}/>
+
                     <img src={Biased} alt="biased-img" className="critical-img" 
-                        onClick={() => setActiveInfo('Biased - AI can reflect biases present in its training data,leading to unfair or misleading results.')}/>
+                        onClick={() => {
+                            setActiveInfo('Biased - AI can reflect biases present in its training data,leading to unfair or misleading results.');
+                            showInfo();
+                        }}/>
                     <img src={ContextAwerness} alt="context-awerness-img" className="critical-img"
-                        onClick={() => setActiveInfo('Contextawerness -  AI does not understand situations, emotions, or intentions the way humans do.')}/>
+                        onClick={() => {
+                            setActiveInfo('Contextawerness -  AI does not understand situations, emotions, or intentions the way humans do.');
+                            showInfo();
+                        }}/>
                     <img src={DataQuality} alt="data-quality-img" className="critical-img" 
-                        onClick={() => setActiveInfo('Dataquality - The quality of AI results depends on the quality of the data behind them.')}/>
+                        onClick={() => {
+                            setActiveInfo('Dataquality - The quality of AI results depends on the quality of the data behind them.');
+                            showInfo();
+                        }}/>
                     <img src={Accountability} alt="accountability-img" className="critical-img" 
-                        onClick={() => setActiveInfo('Accountability - AI provides suggestions, but humans are responsible for the final decisions.')}/>
+                        onClick={() => {
+                            setActiveInfo('Accountability - AI provides suggestions, but humans are responsible for the final decisions.');
+                            showInfo();
+                        }}/>
 
                 </div>
 
-                <div className="active-info">{activeInfo}</div>
+                <div className="active-info"
+                    style={{display: visible ? 'block' : 'none'}}>
+                    {activeInfo}
+                </div>
 
                 <div className="critical-facit">
                     <h2>Before trusting an AI result, ask yourself:</h2>
