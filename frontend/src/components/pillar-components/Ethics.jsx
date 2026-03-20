1fafb2c041eabb57ffc316748177d59561263c40
eberwild <kevinflotow@arcor.de>
2026-03-21 00:15:00 +0100

    implemetation for ethicPage , first design for TabIcons

--- frontend/src/components/pillar-components/Ethics.jsx
+++ frontend/src/components/pillar-components/Ethics.jsx
@@ -1,8 +1,16 @@
 import MainHeader from "../../components/MainHeader";
 import '../../styles/pillarPages/Ethics.css';
-import Ethic from '../../assets/pillarPages/ethic.png';
+import { useState } from 'react';
+import Fairness from '../../assets/pillarPages/ethic/Fairness.png';
+import Transparency from '../../assets/pillarPages/ethic/Transparency.png';
+import Responsibility from '../../assets/pillarPages/ethic/Responsibility.png';
+import SocialImpact from '../../assets/pillarPages/ethic/Social.png';
+import DataProtection from '../../assets/pillarPages/ethic/Data.png';
 
 function Ethics() {
+
+    const [activeInfo , setActiveInfo] = useState('');
+    const [visible , setVisibility] = useState(false);
 
     return(
 
@@ -17,31 +25,56 @@ function Ethics() {
                     fairness, and accountability in AI,
                     and making conscious,
                     responsible decisions about how AI is developed, deployed, and used.
+                    <br/>
+                    AI systems influence decisions in education, healthcare, work, and everyday life.
+                    Ethics and responsibility help ensure that AI is used in a way that respects human rights,
+                    protects individuals, and benefits society as a whole.
                 </p>
             </div>
 
             <div className="ethic-info">
 
-                <p className="ethic-text">
-                    AI systems influence decisions in education, healthcare, work, and everyday life.
-                    Ethics and responsibility help ensure that AI is used in a way that respects human rights,
-                    protects individuals, and benefits society as a whole.
-                </p>
-
-                <img src={Ethic} 
-                     alt="ethic-overview" 
-                     className="ethic-img"
-                />
+                <div className="ethic-fields">
+                <img src={Fairness} alt="fairness-img" className="fields-img"
+                    onClick={() => {
+                        setActiveInfo('Fairness & Bias - AI systems can produce unfair results if their training data contains biases.');
+                        setVisibility(true);
+                    }}/>
+                
+                <img src={Transparency} alt="transparency-img" className="fields-img"
+                    onClick={() => {
+                        setActiveInfo('Transparency - Users should understand when and how AI is being used, and what its limitations are.');
+                        setVisibility(true);
+                    }}/>
+                
+                <img src={Responsibility} alt="responsibility-img" className="fields-img"
+                    onClick={() => {
+                        setActiveInfo('Responsibility - AI does not make moral decisions — humans remain responsible for outcomes.');
+                        setVisibility(true);
+                    }}/>
+                
+                <img src={SocialImpact} alt="socialImpact-img" className="fields-img" 
+                    onClick={() => {
+                        setActiveInfo('Social Impact - AI can affect jobs, access to information, and social equality.');
+                        setVisibility(true);
+                    }}/>
+                
+                <img src={DataProtection} alt="data-info" className="fields-img" 
+                    onClick={() => {
+                        setActiveInfo('Privacy - AI often relies on large amounts of data, making data protection and privacy essential.');
+                        setVisibility(true);
+                    }}/>
+                                
+                <div className="field-info"
+                    style={{display: visible ? 'block' : 'none'}}>
+                    {activeInfo}
+                </div>
+            </div>
 
             </div>
 
             <div className="ethic-problems">
-                <div className="problem">
-                    <h2>Fairness & Bias</h2>
-                    <p className="problem-text">
-                       AI systems can produce unfair results if their training data contains biases.
-                    </p>
-                </div>
+                
 
                 <div className="problem">
                     <h2>Transparency</h2>
@@ -50,12 +83,7 @@ function Ethics() {
                     </p>
                 </div>
 
-                <div className="problem">
-                    <h2>Privacy</h2>
-                    <p className="problem-text">
-                       AI often relies on large amounts of data, making data protection and privacy essential.
-                    </p>
-                </div>
+                
 
                 <div className="problem">
                     <h2>Human responsibility</h2>
@@ -64,12 +92,7 @@ function Ethics() {
                     </p>
                 </div>
 
-                <div className="problem">
-                    <h2>Social impact</h2>
-                    <p className="problem-text">
-                       AI can affect jobs, access to information, and social equality.
-                    </p>
-                </div>
+                
 
             </div>
 
